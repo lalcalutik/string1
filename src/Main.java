322143c0a0557df8bf178771ce21cb513e7279a2
РОМАН <themorozushka@gmail.com>
2023-01-29 16:33:04 +0300

    Проверьте дз

--- src/Main.java
+++ src/Main.java
@@ -1,26 +1,26 @@
 public class Main {
     public static void main(String[] args) {
         task1();
-        System.out.println();
         task2();
-        System.out.println();
         task3();
-        System.out.println();
-        task4();
-        System.out.println();
-
     }
-
     private static void task1() {
-
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName);
     }
-
     private static void task2() {
+        String firstName = "Ivanov";
+        String middleName = "Ivan";
+        String lastName = "Ivanovich";
+        String fullName = firstName + " " + middleName + " " + lastName;
+        System.out.println(fullName.toUpperCase());
     }
-
     private static void task3() {
-    }
-
-    private static void task4() {
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println(fullName);
     }
 }
